46b6b259567e9b014ab5f68552e008eafbc85b25
sklibad <74158865+sklibad@users.noreply.github.com>
2022-05-16 13:51:49 +0200

    Add files via upload

--- ukol2/load.py
+++ ukol2/load.py
@@ -10,10 +10,12 @@ stops = {}
 stop_times = {}
 trips = {}
 routes = {}
-stop_segments = []
+stop_segments = {}
+
+folder_dir = "C:\\programovani_2\\ProgramovaniII-main\\data_praha\\"
 
 # Load stops to dictionary key = stop_id, item = stop object
-with open('stops.txt', newline='', encoding="utf-8") as data_csv:
+with open(folder_dir + 'stops.txt', newline='', encoding="utf-8") as data_csv:
     reader = csv.DictReader(data_csv, delimiter=',')
     for row in reader:
         stop = Stop()
@@ -21,7 +23,7 @@ with open('stops.txt', newline='', encoding="utf-8") as data_csv:
         stops.update({stop.getID(): stop})
 
 # Load stop times to dictionary key = trip_id, item = stop_time object
-with open('stop_times.txt', newline='', encoding="utf-8") as data_csv:
+with open(folder_dir + 'stop_times.txt', newline='', encoding="utf-8") as data_csv:
     reader = csv.DictReader(data_csv, delimiter=',')
     id = ""
     for row in reader:
@@ -35,17 +37,27 @@ with open('stop_times.txt', newline='', encoding="utf-8") as data_csv:
 
             id = stop_time.getID()
             bus_line_stops = [stop_time]
+    stop_times.update({id: bus_line_stops})
 
 # Load trips to dictionary key = trip_id, item = trip_object
-with open('trips.txt', newline='', encoding="utf-8") as data_csv:
+with open(folder_dir + 'trips.txt', newline='', encoding="utf-8") as data_csv:
     reader = csv.DictReader(data_csv, delimiter=',')
+    id = ""
     for row in reader:
         trip = Trips()
         trip.loadAttributes(row)
-        trips.update({trip.getID(): trip})
+        if trip.getRouteID() == id:
+            route_trips.append(trip)
+        else:
+            if id != "":
+                trips.update({id: route_trips})
+
+            id = trip.getRouteID()
+            route_trips = [trip]
+    trips.update({id: route_trips})
 
 # Load routes to dictionary key = route_id, item = route_object
-with open('routes.txt', newline='', encoding="utf-8") as data_csv:
+with open(folder_dir + 'routes.txt', newline='', encoding="utf-8") as data_csv:
     reader = csv.DictReader(data_csv, delimiter=',')
     for row in reader:
         route = Routes()
@@ -55,28 +67,27 @@ with open('routes.txt', newline='', encoding="utf-8") as data_csv:
 
 for route_ID, route in routes.items():
     bus_line = route.getBusLine()
-    for trip_ID, trip in trips.items():
-        if trip.compareRouteID(route_ID):
-            for stop_time_ID, stop_time in stop_times.items():
-                if stop_time_ID == trip_ID:
-                    for i in range(len(stop_time) - 1):
-                        new_segment = True
-                        where_from = stop_time[i].getStopID()
-                        where_to = stop_time[i+1].getStopID()
-                        for stop_segment in stop_segments:
-                            if stop_segment.compare(where_from, where_to):
-                                stop_segment.addTrip(trip)
-                                stop_segment.addBusLine(bus_line)
-                                new_segment = False
-                        if new_segment:
-                            ss = StopSegment(where_from, where_to)
-                            ss.addTrip(trip)
-                            ss.addBusLine(bus_line)
-                            stop_segments.append(ss)
+    route_trips = trips[route_ID]
+    for trip in route_trips:
+        trip_ID = trip.getID()
+        stop_time = stop_times[trip_ID]
+        for i in range(len(stop_time) - 1):
+            where_from = stop_time[i].getStopID()
+            where_to = stop_time[i+1].getStopID()
+            try:
+                ss = stop_segments[where_from, where_to]
+                ss.addTrip(trip)
+                ss.addBusLine(bus_line)
+            except:
+                ss = StopSegment(where_from, where_to)
+                ss.addTrip(trip)
+                ss.addBusLine(bus_line)
+                stop_segments[where_from, where_to] = ss
+
 
 count_max = 0
 ss_max = None
-for ss in stop_segments:
+for ss in stop_segments.values():
     count = ss.getTripCount()
     if count > count_max:
         count_max = count
